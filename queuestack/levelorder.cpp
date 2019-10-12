0a4f983746ea917a60ca189ded668eb67d60f635
muskankapoor <muskankapoor016@gmail.com>
2019-10-11 21:07:42 -0400

    update

--- queuestack/levelorder.cpp
+++ queuestack/levelorder.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
+struct tree_node{
+
+  tree_node l;
+  tree_node r;
+  int data; 
+};
 
 
-bool func()
+void level_traversal(tree_node t, vector<tree_node> v){
+
+
+  queue<tree_node> q;
+  q.push(t);
+  
+  if (!t){
+    return;
+  }
+
+  while (!q.empty()){
+    v.push_back(q.front());
+    if (q.front()->left){
+      q.push(q.front()->left);
+    }
+    if (q.front()->right){
+      q.push(q.front()->right);
+    }
+    q.pop();
+  }
+  
+}
